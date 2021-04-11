178c1ef56ff0b8d74f74b01de6f2f64ef0bed833
Jinhong <xlffm3@gmail.com>
2021-04-11 14:34:09 +0900

    refactor: add logger test while meeting code convention

--- src/main/java/wunit/testclass/TestClass.java
+++ src/main/java/wunit/testclass/TestClass.java
@@ -1,11 +1,15 @@
 package wunit.testclass;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import wunit.annotation.Test;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class TestClass {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestClass.class);
 
     private final Class<?> testClass;
 
@@ -13,18 +17,22 @@ public class TestClass {
         this.testClass = testClass;
     }
 
-    public void runTest() {
-        Method[] methods = testClass.getDeclaredMethods();
-        for (Method method : methods) {
-            if (method.isAnnotationPresent(Test.class)) {
-                try {
-                    method.invoke(null);
-                } catch (IllegalAccessException e) {
-                    System.out.println("잘못된 사용");
-                } catch (InvocationTargetException e) {
-                    System.out.println("예외 발생");
-                }
-            }
+    public void runTests() {
+        Arrays.stream(testClass.getDeclaredMethods())
+                .forEach(this::runEachTest);
+    }
+
+    private void runEachTest(Method method) {
+        if (!method.isAnnotationPresent(Test.class)) {
+            return;
+        }
+        try {
+            method.invoke(null);
+            LOGGER.info(TestStatus.PASSED.generateLogMessage(testClass.getSimpleName(), method.getName()));
+        } catch (InvocationTargetException invocationTargetException) {
+            LOGGER.info(TestStatus.FAILED.generateLogMessage(testClass.getSimpleName(), method.getName()));
+        } catch (IllegalAccessException illegalAccessException) {
+            LOGGER.info(TestStatus.IGNORED.generateLogMessage(testClass.getSimpleName(), method.getName()));
         }
     }
 }
--- src/test/java/wunit/testclass/TestClassTest.java
+++ src/test/java/wunit/testclass/TestClassTest.java
@@ -1,18 +1,36 @@
 package wunit.testclass;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
 
-import static org.assertj.core.api.Assertions.assertThatCode;
+import java.util.List;
 
-public class TestClassTest {
+import static org.assertj.core.api.Assertions.assertThat;
 
-    @DisplayName("클래스의 WTest 애너테이션이 붙은 메서드들을 검증한다.")
+class TestClassTest {
+
+    @DisplayName("클래스의 Test 애너테이션이 붙은 메서드들을 검증한다.")
     @Test
     void runTest() throws ClassNotFoundException {
+        ListAppender<ILoggingEvent> listAppender = new ListAppender<>();
+        Logger logger = (Logger) LoggerFactory.getLogger(TestClass.class);
+        logger.addAppender(listAppender);
+
+        listAppender.start();
         Class<?> testClassReflection = Class.forName("test.domain.PersonTest");
         TestClass testClass = new TestClass(testClassReflection);
+        testClass.runTests();
 
-        assertThatCode(testClass::runTest).doesNotThrowAnyException();
+        List<ILoggingEvent> list = listAppender.list;
+        String message = list.get(0).getMessage();
+        Level level = list.get(0).getLevel();
+
+        assertThat(message).isEqualTo("[Test Passed] :: PersonTest - move");
+        assertThat(level).isEqualTo(Level.INFO);
     }
 }
